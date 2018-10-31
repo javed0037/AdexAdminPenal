44747f4e1736f048e0d6ed896caf97cc7f67aa27
Javed khan <javedkhan19950@gmail.com>
2018-10-31 08:50:05 +0530

    changes in res

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 var config= {
-    API_URL:"http://139.162.218.18:3002/api/v1/admin",//"http://192.168.1.73:5002/api",//
+    API_URL:"http://localhost:3002/api/v1/admin",
     HOST_URL:"http://localhost:3000"
   
   }
